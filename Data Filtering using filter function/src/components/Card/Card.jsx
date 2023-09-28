cddbc97388c37dc0af2823a67672963f021a897c
metazedge <zunairarif2345@gmail.com>
2023-09-27 18:52:02 -0700

    Update Card.jsx

--- Data Filtering using filter function/src/components/Card/Card.jsx
+++ Data Filtering using filter function/src/components/Card/Card.jsx
@@ -1,24 +1,68 @@
 import "./Card.scss";
 import data from "../../data/data";
-
+import { useEffect, useState } from "react";
 const Card = () => {
+  const [selectedFilters, setSelectedFilters] = useState([]);
+  const [filteredItems, setFilteredItems] = useState(data);
+
+  let filters = ["Mountains", "Waterfall", "Nature", "Forest"];
+
+  const handleClick = (selectedCategory) => {
+    if (selectedFilters.includes(selectedCategory)) {
+      let filters = selectedFilters.filter((el) => el !== selectedCategory);
+      setSelectedFilters(filters);
+    } else {
+      setSelectedFilters([...selectedFilters, selectedCategory]);
+    }
+  };
+
+  useEffect(() => {
+    filterItems();
+  }, [selectedFilters]);
+
+  const filterItems = () => {
+    if (selectedFilters.length > 0) {
+      let tempItems = selectedFilters.map((selectedCategory) => {
+        let temp = data.filter((item) => item.category === selectedCategory);
+        return temp;
+      });
+      setFilteredItems(tempItems.flat());
+    } else {
+      setFilteredItems([...data]);
+    }
+  };
+
   return (
     <div className="card">
-      {data.map(({ id, title, description, image, link }) => {
-        return (
-          <div className="card-container" key={id}>
-            <div className="container">
-              <img src={image} alt="images" loading="lazy" />
+      <div className="btn-container">
+        {filters.map((category, id) => {
+          return (
+            <button
+              onClick={()=>handleClick(category)}
+              className={`button ${
+                selectedFilters?.includes(category) ? "active" : ""
+              }`}
+              key={id}
+            >
+              {category}
+            </button>
+          );
+        })}
+      </div>
+        {filteredItems.map(({ id, title, description, image, link }) => {
+          return (
+            <div className="card-container" key={id}>
+            <div className="container" >
+              <img src={image} alt="images" loading="lazy"/>
               <h3>{title}</h3>
               <p>{description}</p>
               <a href={link}>image link</a>
             </div>
-          </div>
-        );
-      })}
+      </div>
+          );
+        })}
     </div>
   );
 };
 
 export default Card;
-  
